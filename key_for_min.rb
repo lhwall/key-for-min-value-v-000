f5332adf580c3279343ae629481797a7690d068a
Lucy Wall <lhwall@gmail.com>
2018-03-26 13:00:03 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,9 @@ def key_for_min_value(name_hash)
    temp_key = nil
    temp_value = 9999999999999999999999999
    name_hash.each do |key, value|
-     if value =< temp_value 
+     if value <= temp_value 
       temp_key = key
     end 
+  end
   return temp_key
-end
 end
